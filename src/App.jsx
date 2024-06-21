92366eb4de83f356550b22007f7e256cc4c61637
Abhishek <abhishekmeena9171@gmail.com>
2024-06-22 01:04:51 +0530

    changes
    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './components/Nav'
 import First from './components/First'
+import Services from './components/Services'
 
 const App = () => {
   return (
     <div className='bg-black w-full  '>
       <Nav />
       <First />
+     <Services />
     </div>
   )
 }
