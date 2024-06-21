cc95fd634f59da70f9ef4cbe297946c3943f4e9d
shrivasprashant <132904779+shrivasprashant@users.noreply.github.com>
2024-06-22 00:52:25 +0530

    p
    products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './components/Nav'
 import First from './components/First'
+import Products from './components/Products'
 
 const App = () => {
   return (
-    <div className='bg-white w-full  '>
+    <div className='bg-black w-full  '>
       <Nav />
       <First />
+      <Products />
     </div>
   )
 }
